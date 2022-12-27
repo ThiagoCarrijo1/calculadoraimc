6e1ea07101de3a6e9315f51e8f99d271b9ed4963
shaos <thiagocarrijo1@outlook.com>
2022-12-27 11:55:36 -0300

    alterando cores dos resultados do calculo de IMC

--- js/main.js
+++ js/main.js
@@ -10,19 +10,28 @@ function calculaImc() {
 
     var imc = imc1.toFixed(2);
 
-        if(imc <= 16.99) {
-            res.textContent = `Seu IMC é de: ${imc} você está muito abaixo do peso!`;
-    	} else if (imc <= 18.49) {
-    		res.textContent = `Seu IMC é de: ${imc} você está abaixo do peso!`;
-        } else if (imc <= 24.99){
-    		res.textContent = `Seu IMC é de: ${imc} você está com o peso normal!`;
-    	} else if (imc <= 29.99) {
-    		res.textContent = `Seu IMC é de: ${imc} você está acima do peso!`;
-        } else if (imc <= 34.99) {
-    		res.textContent = `Seu IMC é de: ${imc} você está com obesidade tipo 1!`;
-    	} else if (imc <= 39.99) {
-            res.textContent = `Seu IMC é de: ${imc} você está com obesidade tipo 2 (severa)!`;
-        } else {
-            res.textContent = `Seu IMC é de: ${imc} você está com obesidade tipo 3 (mórbida)!`;
-        }
+    if (!peso) {
+      alert("Não foi possível calcular, insira primeiro seu peso e sua altura para saber seu IMC.");
+    } else if (imc <= 16.99) {
+        res.textContent = `Seu IMC é de: ${imc}. Você está muito abaixo do peso e isso pode ser um risco a sua saúde!`;
+        res.classList.add("peso-risco");
+    } else if (imc <= 18.49) {
+    		res.textContent = `Seu IMC é de: ${imc}. Você está abaixo do peso e isso pode ser um risco a sua saúde!`;
+        res.classList.add("peso-risco");
+    } else if (imc <= 24.99){
+    		res.textContent = `Seu IMC é de: ${imc}. Você está com o peso normal! Parabéns continue assim.`;
+        res.classList.add("peso-normal");
+    } else if (imc <= 29.99) {
+    		res.textContent = `Seu IMC é de: ${imc}. Você está acima do peso e isso pode ser um risco a sua saúde!`;
+        res.classList.add("peso-risco");
+    } else if (imc <= 34.99) {
+    		res.textContent = `Seu IMC é de: ${imc}. Você está acima do peso com obesidade tipo 1 e isso pode ser um risco a sua saúde!`;
+        res.classList.add("peso-risco");
+    } else if (imc <= 39.99) {
+        res.textContent = `Seu IMC é de: ${imc}. Você está acima do peso com obesidade tipo 2 (severa) e isso pode ser um risco a sua saúde!`;
+        res.classList.add("peso-risco");
+    } else {
+        res.textContent = `Seu IMC é de: ${imc}. Você está acima do peso com obesidade tipo 3 (mórbida) e isso pode ser um risco a sua saúde!`;
+        res.classList.add("peso-risco");
+    }
 }
